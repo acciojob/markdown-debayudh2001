26e2dd0ef6cbd69a60b4137a559a319f4a68e9c9
debayudh2001 <debayudh31@gmail.com>
2025-04-06 19:18:37 +0530

    third commit

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import "../styles/App.css"
 
 const MarkdownEditor = () => {
     const [input, setInput] = useState("")
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import MarkdownEditor from "./components/MarkdownEditor";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<MarkdownEditor />, document.getElementById("root"))
 
